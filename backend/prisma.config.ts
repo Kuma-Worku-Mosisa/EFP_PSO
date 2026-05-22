c0ab3e31b8fb67369e61c2ae0e26f1037e322f87
ThousandGolder <kumaworku22@gmail.com>
2026-05-22 15:53:03 +0300

    remove sensitive config files from repository

--- backend/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-// filepath: prisma/prisma.config.ts
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-  datasource: {
-    // Hardcode it here for the PULL process
-    url: "sqlserver://localhost:1433;database=EFP_PSO;user=erpuser;password=1234;trustServerCertificate=true",
-  },
-});
